2e35f5977dc5e929e2ad03b47a3ffbb62f37e28b
Anastasia <anstasa.li@gmail.com>
2024-09-23 16:46:39 +0300

    feat add fetching recipes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,36 @@
+import { IRecipe } from '@/types/types';
+import axios from 'axios';
+import { FC, ReactNode, useEffect, useState } from 'react';
 
-export const App = () => {
+export const App:FC = () => {
+  const [recipes, setRecipes] = useState<IRecipe[]>([])
+
+useEffect(() => {
+  fetchRecipes()
+}, [])
+
+async function fetchRecipes() {
+  try {
+    const response = await axios.get<{recipes: IRecipe[]}>('https://dummyjson.com/recipes')
+    setRecipes(response.data.recipes)
+  } catch (error) {
+    alert(error)
+  }
+}
 
   return (
     <div>
       Hello
+      {
+        recipes.map((recipe: IRecipe): ReactNode => {
+        return (
+          <div style={{ width: '300px' }}>
+            <img style={{ width: '100%' }} src={recipe.image} alt={`${recipe.name} image`} />
+            <h4>{recipe.name}</h4>
+          </div>
+        )
+        })
+      }
     </div>
   )
 }
---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export interface IRecipe {
+  id: number
+  name: string
+  ingredients: string[]
+  instructions: string[]
+  servings: number
+  prepTimeMinutes: number
+  cookTimeMinutes: number
+  difficulty: string
+  cuisine: string
+  tags: string[]
+  image: string
+  rating: number
+  mealType: string
+}
